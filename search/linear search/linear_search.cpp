5dc6e857d9ef1172f9f2276be251ee1792b4a57b
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-06 21:11:36 +0300

    linear search main

--- search/linear search/linear_search.cpp
+++ search/linear search/linear_search.cpp
@@ -13,3 +13,25 @@ int linearSearch(int a[],int n,int key){
    return -1;
 
 }
+int main(){
+
+   int n;
+   cin>>n;
+   int a[n];
+   for(int i=0;i<n;i++){
+    cin>>a[i];
+   }
+
+   cout<<"enter key"<<endl;
+   int key;
+   cin>>key;
+
+   int pos = linearSearch(a,n,key);
+   if(pos == -1){
+    cout<<"key not found"<<endl;
+   }else{
+       cout<<"key found at index "<<pos<<endl;
+   }
+
+   return 0;
+}
