fc5ae0dbc2e73554b60caf2674e1aa867382c87d
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-06 21:10:57 +0300

    linear search

---
+++ search/linear search/linear_search.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int linearSearch(int a[],int n,int key){
+
+   for(int i=0;i<n;i++){
+
+    if(a[i] == key){
+        return i;
+    }
+   }
+
+   return -1;
+
+}
