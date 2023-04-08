3173360d5e47bbda2443c966c2c5a4571d0337e6
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-08 20:31:21 +0300

    set up fraction

--- OOP/fractionUse.cpp
+++ OOP/fractionUse.cpp
@@ -5,4 +5,10 @@ using namespace std;
 int main() {
     Fraction f1(10,5);
     Fraction f2(15,4);
+
+    Fraction const f3;
+    cout<<f3.getNumerator()<<" "<<f3.getDenominator();
+
+    f3.setNumerator(10);
+	return 0;
 }
