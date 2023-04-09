d88b14f6de4fc225426b8f2bc0e233f39ab0e316
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-09 22:36:12 +0300

    selection sort main

--- sorting/selectionSort.cpp
+++ sorting/selectionSort.cpp
@@ -34,4 +34,23 @@ void selectionSort(int a[],int n){
      
 }
 
+int main(){
+
+   int n;
+   cin>>n;
+
+   int a[n];
+   for(int i=0;i<n;i++){
+    cin>>a[i];
+   }
+
+   selectionSort(a,n);
+
+   for(int i=0;i<n;i++){
+    cout<<a[i]<<" ";
+   }
+
+    return 0;
+}
+
 
