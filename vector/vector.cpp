959ce33a225a93b017c9a6f211aaa8f9980dd97e
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-09 22:28:58 +0300

    initialize vector

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main(){
     vector<int> *vp = new vector<int>(); /// dynamic
     vector<int> v;   /// static
-
+        vector<int> v2(10,-2);   /// initialise
+    vector<int> v3(100); 
 
    return 0;
 }
