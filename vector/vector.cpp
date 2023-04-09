a4c224bc766c2afa2d452deae017db0a5df8bc63
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-09 22:32:27 +0300

    print vector

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -38,5 +38,21 @@ int main(){
 
     v.pop_back();
 
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<endl;
+    }
+
+    /* cout<<v.at(4)<<endl;
+     cout<<v.at(6)<<endl;
+     */
+
+   /* cout<<v[0]<<endl;
+    cout<<v[1]<<endl;
+    cout<<v[2]<<endl;
+    cout<<v[3]<<endl;
+    cout<<v[4]<<endl;
+    cout<<v[5]<<endl;
+    cout<<v[6]<<endl;*/
+
    return 0;
 }
