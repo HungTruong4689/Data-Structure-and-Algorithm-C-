54eb2ed77b006e78eb5cc910d8635f78414f7258
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-10 17:30:05 +0300

    bubble sort optimize function

--- sorting/bubbleSortOptimizes.cpp
+++ sorting/bubbleSortOptimizes.cpp
@@ -18,3 +18,23 @@ void bubblesort(int a[],int n){
         }
     }
 }
+
+int main(){
+
+   int n;
+   cin>>n;
+
+   int a[n];
+   for(int i=0;i<n;i++){
+    cin>>a[i];
+   }
+
+   bubblesort(a,n);
+
+   for(int i=0;i<n;i++){
+    cout<<a[i]<<" ";
+   }
+
+
+   return 0;
+}
