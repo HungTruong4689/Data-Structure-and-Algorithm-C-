ead7e2260c8e6081750544aed0b22b347a3de36f
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-11 19:31:15 +0300

    isSort main func

--- recursion/checkArraySort.cpp
+++ recursion/checkArraySort.cpp
@@ -42,3 +42,13 @@ bool isSorted3(int a[],int n){
    }
    return isSorted3(a,n-1);
 }
+
+int main(){
+  int a[] = {1,2,3,4,5};
+  if(isSorted(a,5)){
+    cout<<"Sorted"<<endl;
+  }else{
+    cout<<"Not Sorted"<<endl;
+  }
+  return 0;
+}
