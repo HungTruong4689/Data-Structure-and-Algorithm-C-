f3e15d8f7221623c1642c0982643d47a374df387
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-11 19:30:26 +0300

    isSorted 2

--- recursion/checkArraySort.cpp
+++ recursion/checkArraySort.cpp
@@ -17,3 +17,18 @@ bool isSorted(int a[], int n){
    return isSorted(a+1 , n-1);;
 
 }
+
+bool isSorted2(int a[],int n){
+    if(n==0 || n==1){
+       return true;
+   }
+   bool isSmallerSorted = isSorted(a+1 , n-1);
+   if(!isSmallerSorted){
+    return false;
+   }
+   if(a[0] > a[1]){
+    return false;
+   }else{
+     return true;
+   }
+}
