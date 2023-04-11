bd9dab04286690e055957c43eca4da21e3e1c8b4
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-11 19:30:52 +0300

    isSort 3

--- recursion/checkArraySort.cpp
+++ recursion/checkArraySort.cpp
@@ -32,3 +32,13 @@ bool isSorted2(int a[],int n){
      return true;
    }
 }
+
+bool isSorted3(int a[],int n){
+   if(n==0 || n==1){
+       return true;
+   }
+   if(a[n-2] > a[n-1]){
+    return false;
+   }
+   return isSorted3(a,n-1);
+}
