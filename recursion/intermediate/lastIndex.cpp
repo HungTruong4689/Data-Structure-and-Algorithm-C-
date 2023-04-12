c0fe84c832ad5c3c8c4d0d1ea76c6321c2a77d94
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-12 20:05:33 +0300

    last Index 2

--- recursion/intermediate/lastIndex.cpp
+++ recursion/intermediate/lastIndex.cpp
@@ -12,3 +12,15 @@ int lastIndex(int a[],int n,int x,int i){
 
     return lastIndex(a,n,x,i-1);
 }
+
+int lastIndex2(int a[],int n,int x){
+    if(n==0){
+        return -3;
+    }
+
+    if(a[n-1]==x){
+        return n-1;
+    }
+
+    return lastIndex2(a,n-1,x);
+}
