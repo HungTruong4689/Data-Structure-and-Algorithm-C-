21d7ffde54bc068c6b79404107c893334d9cf03f
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-12 20:01:46 +0300

    sumOfArray 2

---
+++ recursion/intermediate/sumOfArray.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int sumOfArray(int a[], int n){
+   if(n==0){
+    return 0;
+   }
+
+   return a[0] + sumOfArray(a+1 , n-1);
+}
+int sumOfArray2(int a[], int n){
+      if(n==0){
+       return 0;
+      }
+     return a[n-1] + sumOfArray2(a,n-1);
+}
