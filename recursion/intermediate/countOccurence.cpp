e4366f462669121bb4588d4b65e85ee043689570
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-13 19:50:52 +0300

    count occurence

--- recursion/intermediate/countOccurence.cpp
+++ recursion/intermediate/countOccurence.cpp
@@ -10,3 +10,12 @@ void printAllPos(int a[],int n,int x,int i){
    }
    printAllPos(a,n,x,i+1);
 }
+void count(int a[],int n,int x,int i, int &ans){
+    if(i==n){
+      return;
+   }
+   if(a[i]==x){
+    ans++;
+   }
+   count(a,n,x,i+1,ans);
+}
