c273739530446b5d61d92e68c2d5ba1947efcab2
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-13 19:52:24 +0300

    save all pos

--- recursion/intermediate/storeAllPos.cpp
+++ recursion/intermediate/storeAllPos.cpp
@@ -11,3 +11,13 @@ void printAllPos(int a[],int n,int x,int i){
    }
    printAllPos(a,n,x,i+1);
 }
+
+void saveAllPos(int a[],int n,int x,int i, vector<int> &ans){
+   if(i==n){
+    return;
+   }
+   if(a[i]==x){
+    ans.push_back(i);
+   }
+   saveAllPos(a,n,x,i+1,ans);
+}
