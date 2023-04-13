8fe2239e4d93d5d80df23852845a154e02e9c021
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-13 19:52:04 +0300

    print all pos

---
+++ recursion/intermediate/storeAllPos.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void printAllPos(int a[],int n,int x,int i){
+   if(i==n){
+    return;
+   }
+   if(a[i]==x){
+    cout<<i<<" ";
+   }
+   printAllPos(a,n,x,i+1);
+}
