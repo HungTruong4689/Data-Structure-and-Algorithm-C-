827ea9743462859ac05fdf3705915a212b816bba
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-13 19:52:52 +0300

    save all pos using array

--- recursion/intermediate/storeAllPos.cpp
+++ recursion/intermediate/storeAllPos.cpp
@@ -21,3 +21,13 @@ void saveAllPos(int a[],int n,int x,int i, vector<int> &ans){
    }
    saveAllPos(a,n,x,i+1,ans);
 }
+int saveAllPosUsingArray(int a[],int n,int x,int i, int out[],int j){
+   if(i==n){
+    return 0;
+   }
+   if(a[i]==x){
+    out[j] = i;
+    return 1 + saveAllPosUsingArray(a,n,x,i+1,out, j+1);
+   }
+   return 0 + saveAllPosUsingArray(a,n,x,i+1,out,j);
+}
