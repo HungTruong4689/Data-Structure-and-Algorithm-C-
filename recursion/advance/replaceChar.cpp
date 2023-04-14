96b1fba1e01dd1a25bdd0dc642a37a3edcd3f2ae
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-14 16:25:07 +0300

    replace Char

--- recursion/advance/replaceChar.cpp
+++ recursion/advance/replaceChar.cpp
@@ -7,3 +7,14 @@ int length(char input[]){
     }
     return 1 + length(input+1);;
 }
+
+void replaceChar(char input[]){
+    if(input[0]=='\0'){
+        return;
+    }
+    if(input[0]=='a'){
+        input[0] = 'x';
+    }
+    replaceChar(input + 1);
+
+}
