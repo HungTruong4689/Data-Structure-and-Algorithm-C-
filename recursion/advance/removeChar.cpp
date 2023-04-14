9cc100200be2859e8b4bc16477b84082fae93d60
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-14 16:33:24 +0300

    removeA

--- recursion/advance/removeChar.cpp
+++ recursion/advance/removeChar.cpp
@@ -7,3 +7,17 @@ int length(char input[]){
     }
     return 1 + length(input+1);;
 }
+
+void removeA(char input[]){
+   if(input[0]=='\0'){
+    return ;
+   }
+   if(input[0]!= 'a'){
+    removeA(input + 1);
+   }else{
+      for(int i=0;input[i]!='\0';i++){
+        input[i] = input[i+1];
+      }
+      removeA(input);
+   }
+}
