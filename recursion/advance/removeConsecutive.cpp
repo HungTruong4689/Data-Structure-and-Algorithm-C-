c3f1fad8575e319390d255b57e21a1da1fe02377
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-14 16:35:42 +0300

    remove consecutive duplicates

--- recursion/advance/removeConsecutive.cpp
+++ recursion/advance/removeConsecutive.cpp
@@ -7,3 +7,18 @@ int length(char input[]){
     }
     return 1 + length(input+1);;
 }
+
+
+void removeConsecutiveDuplicates(char input[]){
+     if(input[0]=='\0'){
+        return;
+     }
+     if(input[0] != input[1]){
+        removeConsecutiveDuplicates(input + 1);
+     }else{
+        for(int i = 0;input[i]!='\0';i++){
+            input[i] = input[i+1];
+        }
+        removeConsecutiveDuplicates(input);
+     }
+}
