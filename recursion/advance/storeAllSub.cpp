f4fc2e68706a886f91a6be0d95b523c9debc72f7
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-19 20:12:00 +0300

    main sub store

--- recursion/advance/storeAllSub.cpp
+++ recursion/advance/storeAllSub.cpp
@@ -10,3 +10,17 @@ void print_subs(string input, string output, vector<string> &v){
     print_subs(input.substr(1), output + input[0],v);  /// include
     print_subs(input.substr(1), output,v);   /// exclude
 }
+
+int main(){
+  string in;
+  cin>>in;
+  string out = "";
+
+   vector<string> v;
+  print_subs(in, out, v);
+   for(int i=0;i<v.size();i++){
+    cout<<v[i]<<endl;
+   }
+
+  return 0;
+}
